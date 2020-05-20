666f9b20e49abfd6743a1266a0637d7565319255
Rizan Zaky <rizan149@gmail.com>
2020-05-20 20:55:50 +0530

    pray calendar with data flicker

--- src/XamForms/PrayCalendar/PrayCalendar.Android/MainActivity.cs
+++ src/XamForms/PrayCalendar/PrayCalendar.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace PrayCalendar.Droid
 {
-    [Activity(Label = "PrayCalendar", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Pray Calendar", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
